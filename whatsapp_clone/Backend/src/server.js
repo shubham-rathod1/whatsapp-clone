b333b9e2cdf74e086ab5815cbc353f7c3206990f
shubham <shubs12@gmail.com>
2021-09-16 00:03:45 +0530

    backend setup

---
+++ whatsapp_clone/Backend/src/server.js
@@ -0,0 +1,6 @@
+const app = require("./index");
+// const connect = require("")
+
+app.listen(2244, () => {
+    console.log("connected to port 2244");
+})
---
+++ whatsapp_clone/Frontend/src/App.js
@@ -0,0 +1,6 @@
+function App() {
+  return (<div>
+    <h2>my whatsapp app</h2>
+  </div>);
+}
+export default App;
